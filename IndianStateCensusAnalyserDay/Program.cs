e25f25e18a925dfc0cac25170ee11cdc4c11b48a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-01 19:04:23 +0530

    Added welcome to Indian State Census Analyser program

---
+++ IndianStateCensusAnalyserDay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserDay29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian state census analyser");
+            Console.ReadLine();
+        }
+    }
+}
